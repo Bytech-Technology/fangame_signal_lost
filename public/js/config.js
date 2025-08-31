643db667c5d70dd5af0532fb31a782e37d7ffa44
Bytech-Technology <bytechsuppor@gmail.com>
2025-08-31 20:28:21 -0300

    feat(core): add SETTINGS state to GAME_STATES

--- public/js/config.js
+++ public/js/config.js
@@ -2,6 +2,7 @@ export const GAME_STATES ={
     MENU: "menu",
     WARNING: "warning",
     OFFICE: "office",
+    SETTINGS: "Settings",
     CREDITS: "credits",
     NEXT_DAY: "next_day",
     GAME_OVER: "game_over"
